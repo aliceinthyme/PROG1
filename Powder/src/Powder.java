9733e844416d89b8ac297c72edd39f616092bf62
aliceinthyme <alice.neuhaus@protonmail.com>
2024-12-17 18:05:09 +0100

    does x^n % a

--- Powder/src/Powder.java
+++ Powder/src/Powder.java
@@ -1,5 +1,13 @@
 public class Powder {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println(pow(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+    }
+    static int pow(int x, int k, int n) {
+        int temp = 1;
+        for(int i = 1; i <= k; i++) {
+            temp *= x;
+            temp = temp % n;
+        }
+        return(temp);
     }
 }
