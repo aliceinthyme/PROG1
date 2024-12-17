c0cf79645abc0da5fec6cb793d560833336cd44f
aliceinthyme <alice.neuhaus@protonmail.com>
2024-12-17 09:05:35 +0100

    init files

---
+++ Complex/src/Complex.java
@@ -0,0 +1,5 @@
+public class Complex {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Powder/src/Powder.java
@@ -0,0 +1,5 @@
+public class Powder {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
