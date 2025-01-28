07989270cedc3b6339cbed477ae0488a5da38847
aliceinthyme <alice.neuhaus@protonmail.com>
2025-01-28 15:35:09 +0100

    Generated Projects

---
+++ Uebung09/Digits/src/Digits.java
@@ -0,0 +1,5 @@
+public class Digits {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Uebung09/TestBrackets/src/Brackets.java
@@ -0,0 +1,5 @@
+public class Brackets {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
