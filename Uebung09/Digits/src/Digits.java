44c163dd3b9102470e83cb25219cb861ea021d0d
aliceinthyme <alice.neuhaus@protonmail.com>
2025-01-28 18:12:07 +0100

    Digits done, brackets started but ehhhh

--- Uebung09/Digits/src/Digits.java
+++ Uebung09/Digits/src/Digits.java
@@ -1,5 +1,53 @@
 public class Digits {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a = Integer.parseInt(args[0]);
+        int b = 0;
+        System.out.println("Input Number: "+a);
+        while(a != 0) {
+            int temp = a % 10;
+            b *= 10;
+            b += temp;
+            a -= temp;
+            if(a != 0) {
+                a /= 10;
+            }
+        }
+        System.out.print("Seperated: ");
+        while(b != 0) {
+            int temp = b % 10;
+            b -= temp;
+            if(b != 0) {
+                b /= 10;
+            }
+            switch (temp) {
+                case 1:
+                    System.out.print("one ");
+                    break;
+                case 2:
+                    System.out.print("two ");
+                    break;
+                case 3:
+                    System.out.print("three ");
+                    break;
+                case 4:
+                    System.out.print("four ");
+                    break;
+                case 5:
+                    System.out.print("five ");
+                    break;
+                case 6:
+                    System.out.print("six ");
+                    break;
+                case 7:
+                    System.out.print("seven ");
+                    break;
+                case 8:
+                    System.out.print("eight ");
+                    break;
+                case 9:
+                    System.out.print("nine ");
+                    break;
+            }
+        }
     }
 }
--- Uebung09/TestBrackets/src/Brackets.java
+++ Uebung09/TestBrackets/src/Brackets.java
@@ -1,5 +1,95 @@
 public class Brackets {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String s = args[0];
+        if(testBrackets(s)) {
+            System.out.println("Correct expression");
+        } else {
+            System.out.println("False expression");
+        }
+    }
+    public static boolean testBrackets(String s) {
+        int rBr = 0;
+        int sBr = 0;
+        int cBr = 0;
+        for(int i = 0; i < s.length(); i++) {
+            System.out.println("i: "+i);
+            char a = s.charAt(i);
+            switch (a) {    //checks amount of brackets
+                case '(':
+                    rBr += 1;
+                    break;
+                case ')':
+                    rBr -= 1;
+                    if(!checkOpen(s, '(', i)) {
+                        return(false);
+                    }
+                    break;
+                case '[':
+                    sBr += 1;
+                    break;
+                case ']':
+                    sBr -= 1;
+                    if(!checkOpen(s, '[', i)) {
+                        return(false);
+                    }
+                    break;
+                case '{':
+                    cBr += 1;
+                    break;
+                case '}':
+                    cBr -= 1;
+                    if(!checkOpen(s, '{', i)) {
+                        return(false);
+                    }
+                    break;
+            }
+        }
+        if(rBr == 0 && cBr == 0 && sBr == 0) {
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+    public static boolean checkOpen(String s, char a, int b) {
+        b--;
+        int rBr = 0;
+        int sBr = 0;
+        int cBr = 0;
+        while(s.charAt(b) != a) {
+            char bchar = s.charAt(b);
+            System.out.println(bchar);
+            switch (bchar) {    
+                case '(':
+                    rBr -= 1;
+                    break;
+                case ')':
+                    rBr += 1;
+                    break;
+                case '[':
+                    sBr -= 1;
+                    break;
+                case ']':
+                    sBr += 1;
+                    break;
+                case '{':
+                    cBr -= 1;
+                    break;
+                case '}':
+                    cBr += 1;
+                    break;
+            }
+            b--;
+            System.out.println(rBr+" "+sBr+" "+cBr);
+        }
+        if(rBr == 0 && cBr == 0 && sBr == 0) {
+            return(true);
+        } else {
+            return(false);
+        }
     }
 }
+
+/*
+    ((([ )))]
+ */
